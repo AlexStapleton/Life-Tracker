aa6a14c0247730eac0b004ec1969faed8c408ea1
Alex Stapleton <alexandermstapleton@gmail.com>
2021-02-04 12:30:33 -0800

    First draft of main_menu interface

--- config.py
+++ config.py
@@ -10,3 +10,17 @@ bool_false_possible_values = ['0', 'false', 'False', 'f', 'F']
 
 # Types of trackers supported
 tracker_types = ['num', 'n', 'bool', 'b', 'string', 's']
+
+# Main Menu actions, used by interface_funcs.py
+main_menu_actions = {"1":"Add new tracker",
+                     "2":"Remove a tracker",
+                     "3":"Log to a tracker",
+                     "4": "View trackers",
+                     "5":"View tracker values"}
+
+# Calling the function based on main menu selection
+main_menu_functions = {"1": "core_funcs.add_tracker_item()",
+                       "2": "database_funcs.remove_master_tracker()",
+                       "3": "core_funcs.add_transaction()",
+                       "4": "database_funcs.list_active_trackers()",
+                       "5": "database_funcs.list_transactions_tracker()"}
--- interface_funcs.py
+++ interface_funcs.py
@@ -1,2 +1,32 @@
 # Functions for selecting actions
 
+# Standard packages
+
+# Program packages
+import support_funcs
+import config
+import core_funcs
+import database_funcs
+
+def main_menu():
+    """Main user menu that program will revert to each time an action is completed"""
+    print("""________ Main Menu ________ \nPlease select one of the\nfollowing actions \n""")
+
+    # Update the main menu potential choices
+    menu_vals = []
+
+    for x in config.main_menu_actions:
+        menu_vals.append(x)
+
+    # Iterate through the main menu selection values and print
+
+    for x in config.main_menu_actions:
+        print(x ,"-", config.main_menu_actions[x])
+
+    # Get the users input
+    selection = ""
+
+    while selection not in menu_vals:
+        selection = input("Selection: ")
+
+    return selection
