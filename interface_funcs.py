aea52a0a327799e2f5a25bb22db08d5cae9c78b7
Alex Stapleton <alexandermstapleton@gmail.com>
2021-02-04 20:08:24 -0800

    update to interface_funcs

--- interface_funcs.py
+++ interface_funcs.py
@@ -1,2 +1,25 @@
 # Functions for selecting actions
+# Standard packages
 
+# Program packages
+import support_funcs
+import config
+import core_funcs
+import database_funcs
+
+def main_menu():
+    """Main user menu that program will revert to each time an action is completed"""
+    print("""________ Main Menu ________ \nPlease select one of the\nfollowing actions \n""")
+
+    # Grabs the list of available main_menu options, and prints it
+    menu_vals = support_funcs.id_print_dict(config.main_menu_actions)
+
+    # Get the users input
+    selection = ""
+
+    while selection not in menu_vals:
+        selection = input("Selection: ")
+        if selection == "q" or "quit":
+            exit()
+
+    return selection
